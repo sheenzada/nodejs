3ed14143dac75d388f77052171314e0a20970bf0
Inamullah <sheenzada7@gmail.com>
2026-03-03 19:33:04 -0800

    node basics

--- Youtube lec/basics.js
+++ Youtube lec/basics.js
@@ -1,52 +1,57 @@
-const { log } = require("node:console");
+const data = require('./data');
 
-var a = 100;
-let b = 20;
-const c = 50
+console.log(data);
 
-b = 100;
-a = 200;
-// c = 300;
-console.log(a+b+c);
 
-if(a==200){
-    console.log("This is if condition");
-}
-else{
-    console.log("This is else condition");
+// const { log } = require("node:console");
+
+// var a = 100;
+// let b = 20;
+// const c = 50
+
+// b = 100;
+// a = 200;
+// // c = 300;
+// console.log(a+b+c);
+
+// if(a==200){
+//     console.log("This is if condition");
+// }
+// else{
+//     console.log("This is else condition");
     
-}
+// }
 
-function fruits(item){
-    console.log("this is "+item);
+// function fruits(item){
+//     console.log("this is "+item);
     
-}
-console.log("banana");
-console.log("apple");
+// }
+// console.log("banana");
+// console.log("apple");
 
 
-// for (a=0; a<=10; a++){
+// // for (a=0; a<=10; a++){
+// //     console.log(a);
+    
+// // }
+
+// var a = 0
+
+// while(a<=8){
+//     a++
 //     console.log(a);
     
 // }
 
-var a = 0
-
-while(a<=8){
-    a++
-    console.log(a);
+// var users = ['inam','akash','yaseen','tajwar']
+// for (a=0; a<users.length; a++){
+//     console.log(users[a]);
     
-}
+// }
 
-var users = ['inam','akash','yaseen','tajwar']
-for (a=0; a<users.length; a++){
-    console.log(users[a]);
-    
-}
-
-var user = {
-    name:'InamUllah',
-    City:'Skardu',
-    Age:18
-}
-console.log(user.Age);
+// var user = {
+//     name:'InamUllah',
+//     City:'Skardu',
+//     Age:18
+// }
+// console.log(user.Age);
